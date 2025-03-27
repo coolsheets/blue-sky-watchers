030bb8dae3cb102d31bb41c5f0b371f0a7b28a7a
shaneinceptionu <shane@inceptionu.com>
2025-03-27 15:36:06 -0600

    added super hero routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import superHeroRoutes from './routes/superHeroRoutes.js'
 
 const app = express();
 const PORT = 3000;
+
+app.use('/superheroes', superHeroRoutes);
 
 app.get('/burger', function (req, res) {
     res.send('Ah welcome to our resturant, we have the best burgers in town! but I cant make any right now');
@@ -10,6 +13,7 @@ app.get('/burger', function (req, res) {
 app.get('/fries', function (req, res) {
     res.send('Ah welcome to our resturant, we have the best fries in town! but I cant make any right now');
 })
+
 
 const server = app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
