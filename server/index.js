91068828dfa53be255e1700222e23f6f8df0a59b
shaneinceptionu <shane@inceptionu.com>
2025-03-27 15:07:23 -0600

    added a test endpoint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-import * as express from 'express';
+import express from 'express';
 
-// const app = express();
+const app = express();
+const PORT = 3000;
+// console.log("Welcome to mars, earthings!");
 
-console.log("Welcome to mars, earthings!");
+app.get('/burger', function (req, res) {
+    res.send('Ah welcome to our resturant, we have the best burgers in town! but I cant make any right now');
+})
+
+const server = app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT)
+})
