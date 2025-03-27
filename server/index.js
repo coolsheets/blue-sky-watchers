afe336bf0ac325e45a603e18d2241a2cf91702a4
shaneinceptionu <shane@inceptionu.com>
2025-03-27 15:15:17 -0600

    added a second test endpoint

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
-// console.log("Welcome to mars, earthings!");
 
 app.get('/burger', function (req, res) {
     res.send('Ah welcome to our resturant, we have the best burgers in town! but I cant make any right now');
+})
+
+app.get('/fries', function (req, res) {
+    res.send('Ah welcome to our resturant, we have the best fries in town! but I cant make any right now');
 })
 
 const server = app.listen(PORT, () => {
